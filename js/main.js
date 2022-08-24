c961f7fb16e55992ea02cfb2607ff0a73ee2280c
Aldana Martin <aldimartin1990@gmail.com>
2022-08-23 21:34:37 -0300

    Se realizaron cambios en el trabajo

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
-
 let usuario = "Aldana"
 let contraseña = "1234"
 
 for (i=0; i<3; i++) {
-   usuarioIngresado= prompt ("Ingresa tu usuario");
-   contraseñaIngresada = prompt ("Ingresa tu contraseña"); 
-   
-   if (usuario==usuarioIngresado && contraseña== contraseñaIngresada) {
-    console.log("Ingreso exitoso");
-    alert ("Bienvenida " + usuario  + "," + " has ingresado con éxito");
-    break;
-   } else if (usuario!=usuarioIngresado || contraseña!=contraseñaIngresada) {
-    console.log("Usuario o contraseña incorrecta");
-    alert ("Alguno de los datos es incorrecto");
-   } 
-}
-validacion = false
+
+    let usuarioIngresado = prompt("Ingrese su usuario");
+    let contraseñaIngresada = prompt("Ingrese su contraseña");
+
+    if (usuarioIngresado == usuario && contraseña == contraseñaIngresada) {
+        console.log("Ingreso exitoso")
+        alert("Bienvenida " + usuario + "," + " has ingresado con éxito");
+        validacion = true;
+        break;
+    }
+    else if (usuarioIngresado !== usuario || contraseñaIngresada !== contraseña) {
+            console.log("Usuario o contraseña incorrecto");
+            alert("Usuario o contraseña incorrecto");
+            validacion = false;
+        }
+    } 
 if (validacion == false) {
-    alert ("Superaste el límite de intentos");
+    console.log("Superó el límite de intentos")
+    alert("Superó el limite de intentos")
 }
+
